5659e63047f026f4e5d4faa015a7e0b240bc58e6
Domen Hren <hrnedomen27@gmail.com>
2020-05-29 15:58:27 +0200

    Zavarovanje pred istalacijo Tailwinda

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')   
     <h1>Create a Project</h1>  
     
-    <form method="POST" class="container" action="/projects" style="padding-top: 40px">
+    <form method="POST"  action="/projects">
         @csrf
 
         <div>
@@ -35,9 +28,9 @@
         <div>
             <div>
                 <button type="submit">Create Project</button>
+                <a href="/projects">Cancle</a>
             </div>
         </div>
 
     </form>
-</body>
-</html>
+@endsection
