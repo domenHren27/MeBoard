697ced3aa202e53b3fc9e7a372e7bbc8b6d4436c
Domen Hren <hrnedomen27@gmail.com>
2020-07-13 22:05:25 +0200

    WIP

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,10 +1,10 @@
 
- <div class="bg-white p-5 rounded-lg shadow shadow" style="height: 200px;">
+ <div class="bg-white p-5 rounded-lg shadow shadow flex flex-col" style="height: 200px;">
     <h3 class="font-normal text-xl py-5 -ml-5 border-l-4 border-blue-light pl-4 mb-4">
         <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
     </h3>
 
-    <div class="text-grey-dark mb-4"> {{ Str::limit($project->description, 100) }} </div>
+    <div class="text-grey-dark mb-4 flex-1"> {{ Str::limit($project->description, 100) }} </div>
 
     <footer>
         <form method="POST" action="{{ $project->path() }}" class="text-right">
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -102,6 +102,30 @@
                 @include('projects.card')
 
                 @include('projects.activity.card')
+
+                <div class="bg-white p-5 rounded-lg shadow shadow flex flex-col mt-3" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-5 -ml-5 border-l-4 border-blue-light pl-4 mb-4">
+                        Invite a User
+                    </h3>
+            
+                    <form method="POST" action="{{ $project->path() .'/invitations' }}">
+                        @csrf
+
+                        <div class="mb-3">
+                            <input type="email" name="email" class="border border-grey rounded w-full py-2 px-3" placeholder="Email Adress">
+                        </div>
+                        
+
+                        <button class="py-2 px-5" type="submit" style=
+                        "background-color: #47cdff; 
+                        text-decoration: none;
+                        box-shadow: 0 2px 7px 0 #b0eaff; 
+                        border-radius: 5rem;
+                        color: white;
+                        font-size: 0.8rem;">Invite here</button>
+                    </form>                 
+                
+                </div>
                 
             </div>
         </div>
